60a7e6b6519a325ba12c1ba1aeabb9d3e5fefa43
Josh Bacon <bacon.josh09@gmail.com>
2021-11-26 02:16:26 -0500

    Add game specific insights to insight type

--- src/models/Insights.ts
+++ src/models/Insights.ts
@@ -1,90 +1,107 @@
-import { ISerializable } from './ISerializable';
-import Match from './Match';
+import { ISerializable } from "./ISerializable";
+import Match from "./Match";
+import { GameSpecificInsights } from "./game-specifics/InsightsData";
 
 export default class Insights implements ISerializable {
-  private _highScoreMatch: Match | null;
-  private _averageMatchScore: number;
-  private _averageWinningScore: number;
-  private _averageWinningMargin: number;
-  private _averageMajorPenalties: number;
-  private _averageMinorPenalties: number;
+    private _highScoreMatch: Match | null;
+    private _averageMatchScore: number;
+    private _averageWinningScore: number;
+    private _averageWinningMargin: number;
+    private _averageMajorPenalties: number;
+    private _averageMinorPenalties: number;
+    private _game: GameSpecificInsights | undefined;
 
-  constructor() {
-    this._highScoreMatch = null;
-    this._averageMatchScore = 0;
-    this._averageWinningScore = 0;
-    this._averageWinningMargin = 0;
-    this._averageMajorPenalties = 0;
-    this._averageMinorPenalties = 0;
-  }
+    constructor() {
+        this._highScoreMatch = null;
+        this._averageMatchScore = 0;
+        this._averageWinningScore = 0;
+        this._averageWinningMargin = 0;
+        this._averageMajorPenalties = 0;
+        this._averageMinorPenalties = 0;
+    }
 
-  toJSON(): object {
-    return {
-      high_score_match: this.highScoreMatch ? this.highScoreMatch.toJSON() : null,
-      average_match_score: this.averageMatchScore,
-      average_winning_score: this.averageWinningScore,
-      average_winning_margin: this.averageWinningMargin,
-      average_major_penalty: this.averageMajorPenalties,
-      average_minor_penalty: this.averageMinorPenalties
-    };
-  }
+    toJSON(): object {
+        return {
+            high_score_match: this.highScoreMatch
+                ? this.highScoreMatch.toJSON()
+                : null,
+            average_match_score: this.averageMatchScore,
+            average_winning_score: this.averageWinningScore,
+            average_winning_margin: this.averageWinningMargin,
+            average_major_penalty: this.averageMajorPenalties,
+            average_minor_penalty: this.averageMinorPenalties,
+        };
+    }
 
-  fromJSON(json: any): Insights {
-    const insights = new Insights();
-    insights.highScoreMatch = json.high_score_match ? new Match().fromJSON(json.high_score_match) : null;
-    insights.averageMatchScore = json.average_match_score;
-    insights.averageWinningScore = json.average_winning_score;
-    insights.averageWinningMargin = json.average_winning_margin;
-    insights.averageMajorPenalties = json.average_major_penalty;
-    insights.averageMinorPenalties = json.average_minor_penalty;
-    return insights;
-  }
+    fromJSON(json: any): Insights {
+        const insights = new Insights();
+        insights.highScoreMatch = json.high_score_match
+            ? new Match().fromJSON(json.high_score_match)
+            : null;
+        insights.averageMatchScore = json.average_match_score;
+        insights.averageWinningScore = json.average_winning_score;
+        insights.averageWinningMargin = json.average_winning_margin;
+        insights.averageMajorPenalties = json.average_major_penalty;
+        insights.averageMinorPenalties = json.average_minor_penalty;
+        if ("game" in json) {
+            insights.game = json.game;
+        }
+        return insights;
+    }
 
-  get highScoreMatch(): Match | null {
-    return this._highScoreMatch;
-  }
+    get highScoreMatch(): Match | null {
+        return this._highScoreMatch;
+    }
 
-  set highScoreMatch(value: Match | null) {
-    this._highScoreMatch = value;
-  }
+    set highScoreMatch(value: Match | null) {
+        this._highScoreMatch = value;
+    }
 
-  get averageMatchScore(): number {
-    return this._averageMatchScore;
-  }
+    get averageMatchScore(): number {
+        return this._averageMatchScore;
+    }
 
-  set averageMatchScore(value: number) {
-    this._averageMatchScore = value;
-  }
+    set averageMatchScore(value: number) {
+        this._averageMatchScore = value;
+    }
 
-  get averageWinningScore(): number {
-    return this._averageWinningScore;
-  }
+    get averageWinningScore(): number {
+        return this._averageWinningScore;
+    }
 
-  set averageWinningScore(value: number) {
-    this._averageWinningScore = value;
-  }
+    set averageWinningScore(value: number) {
+        this._averageWinningScore = value;
+    }
 
-  get averageWinningMargin(): number {
-    return this._averageWinningMargin;
-  }
+    get averageWinningMargin(): number {
+        return this._averageWinningMargin;
+    }
 
-  set averageWinningMargin(value: number) {
-    this._averageWinningMargin = value;
-  }
+    set averageWinningMargin(value: number) {
+        this._averageWinningMargin = value;
+    }
 
-  get averageMajorPenalties(): number {
-    return this._averageMajorPenalties;
-  }
+    get averageMajorPenalties(): number {
+        return this._averageMajorPenalties;
+    }
 
-  set averageMajorPenalties(value: number) {
-    this._averageMajorPenalties = value;
-  }
+    set averageMajorPenalties(value: number) {
+        this._averageMajorPenalties = value;
+    }
 
-  get averageMinorPenalties(): number {
-    return this._averageMinorPenalties;
-  }
+    get averageMinorPenalties(): number {
+        return this._averageMinorPenalties;
+    }
 
-  set averageMinorPenalties(value: number) {
-    this._averageMinorPenalties = value;
-  }
+    set averageMinorPenalties(value: number) {
+        this._averageMinorPenalties = value;
+    }
+
+    get game(): GameSpecificInsights | undefined {
+        return this._game;
+    }
+
+    set game(value: GameSpecificInsights | undefined) {
+        this._game = value;
+    }
 }
